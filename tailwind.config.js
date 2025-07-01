d44c6cb6f16c378df9af469e8857a867cd6ec0c8
Gaurang Ambasana <gpambasana@gmail.com>
2025-07-01 13:35:33 +0530

    feat: add Tailwind CSS configuration and global styles

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // if you use app dir
+    "./components/**/*.{js,ts,jsx,tsx}", // components dir
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
