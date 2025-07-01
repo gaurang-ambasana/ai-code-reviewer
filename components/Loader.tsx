9013d3fd9479c783e333b38e35e8e59d125b8bb6
Gaurang Ambasana <gpambasana@gmail.com>
2025-07-01 13:46:55 +0530

    refactor: remove CSS module styles from Loader component and use global styles

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,10 +1,9 @@
 import { type FC } from "react";
-import styles from "./Loader.module.css";
 
 const Loader: FC = () => (
-  <div className={styles.loader}>
-    <div className={styles.spinner}></div>
-    <div className={styles.loadingText}>Generating Review...</div>
+  <div className="loader">
+    <div className="spinner"></div>
+    <div className="loading-text">Generating Review...</div>
   </div>
 );
 
